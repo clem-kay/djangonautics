726210addaeb74452ccfbccaac7d708520bb6c30
clement <ckoomson75@gmail.com>
2020-05-17 11:29:57 +0000

    Images in django

--- djangonautics/articles/urls.py
+++ djangonautics/articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('',views.article_list),
+    path('',views.article_list,name="list"),
     path('^(?P<slug>[\W-]+)/$',views.article_details),
 ]
